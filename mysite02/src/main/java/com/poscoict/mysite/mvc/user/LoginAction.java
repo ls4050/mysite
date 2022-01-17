1df67170c5130e4ac0814901343d78d56be9d2bd
Wonseok <ls4050@naver.com>
2022-01-17 10:51:02 +0900

    mysite02 - user#3

--- mysite02/src/main/java/com/poscoict/mysite/mvc/user/LoginAction.java
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/LoginAction.java
@@ -11,26 +11,28 @@ import com.poscoict.mysite.vo.UserVo;
 import com.poscoict.web.mvc.Action;
 import com.poscoict.web.util.MvcUtil;
 
-public class JoinAction implements Action {
+public class LoginAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name = request.getParameter("name");
+		// TODO Auto-generated method stub
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		String gender = request.getParameter("gender");
 		
-		UserVo vo = new UserVo();
-		vo.setName(name);
-		vo.setEmail(email);
-		vo.setPassword(password);
-		vo.setGender(gender);
+		//인증받은 유저
+		UserVo authUser = new UserDao().findByEmailAndPassword(email, password);
+		
+		if(authUser == null) {
+			/* 이메일 또는 비밀번호가 틀림 */
+			request.setAttribute("result", "fail");
+			request.setAttribute("email", email);
+			MvcUtil.forward("user/loginform", request, response);
+			return; //return 해서 끝내줘야한다 아니면 아래 코드 실행된다.
+		}
+		
+		//인증 처리(Session 처리)
 		
 		
-		
-		new UserDao().insert(vo);
-		
-		MvcUtil.redirect(request.getContextPath()+"/user?a=joinsuccess", request, response);
 	}
 
 }
