52bf9bad64b46e40e1cc56009d1bcf4fd132b735
Wonseok <ls4050@naver.com>
2022-01-19 02:12:26 +0900

    get "rVo" -> req.set "rVo"-> writeaction

--- mysite02/src/main/java/com/poscoict/mysite/mvc/board/WriteFormAction.java
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/board/WriteFormAction.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.poscoict.mysite.vo.BoardVo;
 import com.poscoict.web.mvc.Action;
 import com.poscoict.web.util.MvcUtil;
 
@@ -13,6 +14,10 @@ public class WriteFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getAttribute("rVo") != null) {
+			BoardVo rVo = (BoardVo)request.getAttribute("rVo");
+			request.setAttribute("rVo", rVo);
+		}
 		MvcUtil.forward("board/write", request, response);
 	}
 
