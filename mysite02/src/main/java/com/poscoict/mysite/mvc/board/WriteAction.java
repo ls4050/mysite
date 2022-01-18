ee6ce2e661be33d30ffc9a2d1d5d2c3aea33fa5f
Wonseok <ls4050@naver.com>
2022-01-19 02:11:00 +0900

    gno, ono, depth -> reply controll

--- mysite02/src/main/java/com/poscoict/mysite/mvc/board/WriteAction.java
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/board/WriteAction.java
@@ -17,22 +17,28 @@ public class WriteAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String g_no = request.getParameter("g_no");
+		String o_no = request.getParameter("o_no");
+		String depth = request.getParameter("depth");
 		String title = request.getParameter("title");
 		String contents = request.getParameter("content");
 		HttpSession session = request.getSession();
 		UserVo authUser = (UserVo) session.getAttribute("authUser");
-		
-		//가입 안했을 때 컨트롤해줘야한다 
 		BoardVo vo = new BoardVo();
+		if(!g_no.isBlank()) {
+			//답글
+			vo.setGroupNo(Integer.parseInt(g_no));
+			vo.setOrderNo(Integer.parseInt(o_no));
+			vo.setDepth(Integer.parseInt(depth));
+		} 
+		//새글
+		//가입 안했을 때 컨트롤해줘야한다 
 		vo.setTitle(title);
 		vo.setContents(contents);
 		//null point 
 		vo.setUserNo(authUser.getNo());
-		
-		
 		if(new BoardDao().insert(vo)) {
 			MvcUtil.redirect(request.getContextPath()+"/board", request, response);
 		}
 	}
-
 }
