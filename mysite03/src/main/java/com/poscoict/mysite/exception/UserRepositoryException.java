2087ca8971d750d2ca3f1cbf95882e112267985d
Wonseok <ls4050@naver.com>
2022-01-25 11:06:17 +0900

    mysite03 exception msg add

--- mysite03/src/main/java/com/poscoict/mysite/exception/UserRepositoryException.java
+++ mysite03/src/main/java/com/poscoict/mysite/exception/UserRepositoryException.java
@@ -8,7 +8,7 @@ public class UserRepositoryException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public UserRepositoryException(String message) {
-		
+		super(message);
 	}
 	
 	public UserRepositoryException() {
