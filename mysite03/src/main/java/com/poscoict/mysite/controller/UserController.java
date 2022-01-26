106130847a6f48c77a9ddc579add0f86d242bdbf
Wonseok <ls4050@naver.com>
2022-01-26 15:09:53 +0900

    mysite03 authuser interceptor build

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -5,10 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.poscoict.mysite.service.UserService;
 import com.poscoict.mysite.vo.UserVo;
@@ -36,28 +34,9 @@ public class UserController {
 		return "user/joinsuccess";
 	}
 	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
+	@RequestMapping(value="/login")
 	public String login() {
 		return "user/login";
-	}
-	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String login(
-			HttpSession session,
-			@RequestParam(value="email", required=true, defaultValue="") String email, 
-			@RequestParam(value="password", required=true, defaultValue="") String password,
-			Model model) {
-		UserVo authUser = userService.getUser(email, password);
-		
-		if(authUser == null) {
-			model.addAttribute("result", "fail");
-			model.addAttribute("email", email);
-			return "user/login";
-		}
-		
-		/* 인증처리 */
-		session.setAttribute("authUser", authUser);
-		return "redirect:/";
 	}
 	
 	@RequestMapping(value="/logout", method=RequestMethod.GET)
