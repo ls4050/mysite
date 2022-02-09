7b79dc1f9a9430893df9a44b84f23b3cb83886da
Wonseok <ls4050@naver.com>
2022-02-09 11:09:27 +0900

    package명 수정

--- mysite04/src/main/java/com/poscoict/mysite/config/AppConfig.java
+++ mysite04/src/main/java/com/poscoict/mysite/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.poscoict.mysite.config.app;
+package com.poscoict.mysite.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
